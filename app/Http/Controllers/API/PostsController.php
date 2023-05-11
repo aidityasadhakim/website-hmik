3a342cbae6e2ac5bdffce02c3a2018ad7b8171d7
aidityas <aidityasadhakim250@gmail.com>
2023-05-11 10:41:45 +0700

    Add Create Post API and Update Post API also the Swagger Documentation

--- app/Http/Controllers/API/PostsController.php
+++ app/Http/Controllers/API/PostsController.php
@@ -8,8 +8,8 @@ use App\Models\User;
 use App\Models\Category;
 use App\Helpers\Response;
 use App\Helpers\HttpStatus;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 
 class PostsController extends Controller
@@ -119,10 +119,194 @@ class PostsController extends Controller
         } catch (Exception $e) {
             return Response::error("No Post Found", HttpStatus::$NOT_FOUND);
         }
-        // return view('post', [
-        //     "title" => "Single Post",
-        //     "active" => 'posts',
-        //     "post" => $post
-        // ]);
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/posts/create",
+     *     description="This API allows you to create a new post",
+     *     summary="Create a new post",
+     *     operationId="store",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     description="Post TItle",
+     *                     property="title",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  @OA\Property(
+     *                     description="Post Slug",
+     *                     property="slug",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  @OA\Property(
+     *                     description="Category ID",
+     *                     property="category_id",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  @OA\Property(
+     *                     description="Body of Post",
+     *                     property="body",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  @OA\Property(
+     *                     description="file to upload",
+     *                     property="image",
+     *                     type="file",
+     *                     format="string",
+     *                 ),@OA\Property(
+     *                     description="User ID",
+     *                     property="user_id",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  
+     *                 required={"title","slug","body","image","category_id","user_id"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="successful operation",
+     *         @OA\Schema(ref="#/components/schemas/ApiResponse")
+     *     ),
+     *      tags={
+     *          "posts"
+     *          }
+     * )
+     * */
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'image' => 'image|file|max:1024',
+            'body' => 'required',
+            'user_id' => 'required'
+        ]);
+
+        if ($request->file('image')) {
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
+
+        // $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        $post = Post::create($validatedData);
+
+        if ($post) {
+            return Response::success($post, "Post Created", HttpStatus::$CREATED);
+        } else {
+            return Response::error(HttpStatus::$BAD_REQUEST, "BAD REQUEST");
+        }
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/posts/update/{slug}",
+     *     description="This API allows you to update a new post",
+     *     summary="Update a new post",
+     *     operationId="update",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     description="Post TItle",
+     *                     property="title",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  @OA\Property(
+     *                     description="Post Slug",
+     *                     property="slug",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  @OA\Property(
+     *                     description="Body of Post",
+     *                     property="body",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  @OA\Property(
+     *                     description="file to upload",
+     *                     property="image",
+     *                     type="string",
+     *                     format="string",
+     *                 ),@OA\Property(
+     *                     description="User ID",
+     *                     property="user_id",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     *                  @OA\Property(
+     *                     description="Category ID",
+     *                     property="category_id",
+     *                     type="string",
+     *                     format="string",
+     *                 ),
+     * 
+     *                  
+     *                 required={"title","slug","body","image","user_id","category_id"}
+     *             )
+     *         )
+     *     ),
+     *  @OA\Parameter(
+     *         name="slug",
+     *         in="path",
+     *         description="Slug to retrieve",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="successful operation",
+     *         @OA\Schema(ref="#/components/schemas/ApiResponse")
+     *     ),
+     *      tags={
+     *          "posts"
+     *          }
+     * )
+     * */
+
+    public function update(Request $request, $slug)
+    {
+        try {
+            $rules = [
+                'title' => 'required|max:255',
+                'category_id' => 'required',
+                'image' => 'required',
+                'body' => 'required',
+                'user_id' => 'required',
+            ];
+
+            $validatedData = $request->validate($rules);
+
+            $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+            $updatedPost = Post::where('slug', $slug)
+                ->update($validatedData);
+
+            if ($updatedPost) {
+                return Response::success($updatedPost, "Post Updated", HttpStatus::$CREATED);
+            } else {
+                return Response::error("Post Update Failed", HttpStatus::$EXPECTATION_FAILED);
+            }
+        } catch (Exception $e) {
+            return Response::error($e->getMessage(), HttpStatus::$EXPECTATION_FAILED);
+        }
     }
 }
